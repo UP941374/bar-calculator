638044fdcf4621d77031661bb26b22161a068ea2
UP941374 <up941374@myport.ac.uk>
2021-03-29 13:50:31 +0100

    redone service worker

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,34 @@
-const CACHE_NAME = 'bar-calculator';
+const CACHE_NAME  = 'bar-calculator';
 
-let resourcesToCache = ['./index.html'];
+let resourcesToCache = ["./", "./index.html"];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(resourcesToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.resondWith(
-    caches.match(e.request).then(response = > {
-      return response || fetch(e.request);
-    })
-  );
-});
-
-const cacheWhiteList = ['bar-calculator'];
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        chacheNames.map(cacheName => {
-          if (cacheWhiteList.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            return cache.addAll(resourcesToCache);
         })
-      )
-    })
-  )
-})
+    );
+});
+
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
+
+const cacheWhitelist = ['bar-calculator'];
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
